d30f043a398f53a8261820db5f83346bb63c2232
Akimoto238 <samuelaugustoakimoto@gmail.com>
2022-10-14 10:53:06 -0300

    New Class

--- my-java-progress/src/edu/akimoto/variaveis/ClassVar1.java
+++ my-java-progress/src/edu/akimoto/variaveis/ClassVar1.java
@@ -1,4 +1,9 @@
 package edu.akimoto.variaveis;
 
 public class ClassVar1 {
+    double salarioMinimo = 2500;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numerocurto2 = (short) numeroNormal;
 }
---
+++ my-java-progress/src/edu/akimoto/variaveis/ClassVar2.java
@@ -0,0 +1,7 @@
+package edu.akimoto.variaveis;
+
+public class ClassVar2 {
+    double PI = 3.14;
+
+    double PI = 3.15;
+}
